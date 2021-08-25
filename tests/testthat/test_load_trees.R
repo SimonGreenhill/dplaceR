4e99f13364e976858034a478de2346d893830f62
SimonGreenhill <simon@simon.net.nz>
2021-08-25 13:06:19 +1200

    refactor

--- tests/testthat/test_load_trees.R
+++ tests/testthat/test_load_trees.R
@@ -25,6 +25,13 @@ test_that("test load_trees error invalid renameto", {
 })
 
 
+test_that("test load_trees error no posterior", {
+    expect_error(
+        load_trees("testdata/glottolog_utoa1244", type='posterior'),
+        "Invalid file"
+    )
+})
+
 test_that("test load_trees - glottolog", {
     trees <- load_trees("testdata/glottolog_utoa1244/")
     expect_equal(class(trees), 'multiPhylo')
@@ -71,9 +78,25 @@ test_that("test load_trees - kolipakam - remapping", {
 
 test_that("test load_trees - duplicates in rename", {
     expect_warning(
-        trees <- load_trees("testdata/glottolog_utoa1244_with_duplicates/", renameto="glottocode"),
+        trees <- load_trees(
+            "testdata/glottolog_utoa1244",
+            mappingfile="testdata/glottolog_utoa1244/duplicates.csv",
+            renameto="glottocode"),
         "Removing  3 tips that will be duplicated after rename: bann1248, cent2131, chem1251"
     )
     expect_equal(length(trees[[1]]$tip.label), 3)
     expect_equal(trees[[1]]$tip.label, c('B', 'C', 'A'))  # order in tree newick
+})
+
+
+test_that("test load_trees - multiple society codes", {
+    # e.g. akim1239,akim1239,xd1291,"Ni6, WNAI145"
+    trees <- load_trees("testdata/glottolog_utoa1244", renameto="soc_ids")
+    expect_equal(trees[[1]]$tip.label[[1]], "B144, Nc31, WNAI85, WNAI86, WNAI87")
+})
+
+
+test_that("test load_trees - custom taxa mapping", {
+    trees <- load_trees("testdata/glottolog_utoa1244", mappingfile="testdata/glottolog_utoa1244/custom.csv", renameto="simon")
+    expect_equal(trees[[1]]$tip.label, c("S", "I", "M", "O", "N"))
 })
